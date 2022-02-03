9fb64859d9a1c5a6760c52a52a314d6504fa9126
ahmed00E <afrh20066@gmail.com>
2022-02-03 14:08:45 +0100

    Google places added + commitr

--- frontend/src/components/Calendrier/DateTime.js
+++ frontend/src/components/Calendrier/DateTime.js
@@ -2,19 +2,24 @@ import React from 'react'
 import  {DateTimePikerComponent} from  '@syncfusion/ej2-calendars'
 
 const DateTime = () => {
+  
   return (
      <div>
-       <input type="datetime-local" 
+       <input type="datetime-local"
+        id="meeting-time"
+        name="meeting-time"
+       value="2022-02-02T21:30"
+       min="2022-02-07T00:00" 
+       max="2022-02-14T00:00" 
+       />
+     </div>
         
-       name="time" 
-       value="Dade"
-       placeholder="Type address" 
-       min="" max="" />
        
 
-     </div>
   )
 }
 
 export default DateTime
+
+ 
 
