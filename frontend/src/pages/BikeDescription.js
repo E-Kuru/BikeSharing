c14e0ade7120a77debae03313af1e36a1c146a01
E-Kuru <evandalleaublop@gmail.com>
2022-02-07 17:50:10 +0100

    Indentation

--- frontend/src/pages/BikeDescription.js
+++ frontend/src/pages/BikeDescription.js
@@ -11,123 +11,102 @@ const BikeDescription = () => {
      const Container = styled.div`
            
 `;
-
-
-     const Info = styled.div`
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          padding-left: 30%;
+const Info = styled.div`
+     display: flex;
+     align-items: center;
+     justify-content: center;
+     padding-left: 30%;
 `;
-          
-
-
-
-           
-          
-    const Button = styled.div`
-    color: black;
-    text-decoration: none;
-    font-size: 18px;
-    text-align: center;
-    background-color: white;
-    padding: 5px;
-    border-radius: 30px;
-       width: 100%;
-       display: flex;
-       align-items: center;
-       justify-content: center;
-
- 
-}
-          
+const Button = styled.div`
+     color: black;
+     text-decoration: none;
+     font-size: 18px;
+     text-align: center;
+     background-color: white;
+     padding: 5px;
+     border-radius: 30px;
+     width: 100%;
+     display: flex;
+     align-items: center;
+     justify-content: center;          
 `;
    
-     const Div = styled.div`
-          color: rgb(250, 250, 145);
-          display: block;
-          display: flex;
-          flex-direction: row;
-          flex-wrap: wrap;
-          justify-content: flex-end;
-          align-items: flex-end;
-          align-content: space-around;
-          padding-top: 10px;
-          
+const Div = styled.div`
+     color: rgb(250, 250, 145);
+     display: block;
+     display: flex;
+     flex-direction: row;
+     flex-wrap: wrap;
+     justify-content: flex-end;
+     align-items: flex-end;
+     align-content: space-around;
+     padding-top: 10px;
+     
 `;
           
-    const Span = styled.div`
-          //    display: flex;
-          color: rgb(250, 250, 145);
-          
+const Span = styled.div`
+     //    display: flex;
+     color: rgb(250, 250, 145);
 `;
 
-     const Commentaire = styled.div`
-          margin: auto;
-          width: 80%;
-          border: 2px solid white;
-          padding: 20px;
-          border-radius: 17px;
-          background-color: white;
+const Commentaire = styled.div`
+     margin: auto;
+     width: 80%;
+     border: 2px solid white;
+     padding: 20px;
+     border-radius: 17px;
+     background-color: white;
 `;
           
-     const DvToo = styled.div`
-          margin: auto;
-          width: 80%;
-          border: 2px solid white;
-          padding: 20px;
-          border-radius: 17px;
-          background-color: white;
-          color: white;
-          
+const DvToo = styled.div`
+     margin: auto;
+     width: 80%;
+     border: 2px solid white;
+     padding: 20px;
+     border-radius: 17px;
+     background-color: white;
+     color: white;
+     
  `;
      
-     const DivThree = styled.div`
-          margin: auto;
-          width: 80%;
-          border: 2px solid white;
-          padding: 20px;
-          border-radius: 17px;
-          background-color: white;
+const DivThree = styled.div`
+     margin: auto;
+     width: 80%;
+     border: 2px solid white;
+     padding: 20px;
+     border-radius: 17px;
+     background-color: white;
 `;
     
 
-     const P = styled.div`
-         color: white importn;
-         padding-left: 8%;
-         margin: 40px;
-    
+const P = styled.div`
+     color: white importn;
+     padding-left: 8%;
+     margin: 40px;
+
  `; 
-    const H5 = styled.div`
-         color: black;
+const H5 = styled.div`
+     color: black;
  `;
-    const span = styled.div`
-         color: black;
+const span = styled.div`
+     color: black;
  `;
     
-    const Min = styled.div`
-         display: flex;
+const Min = styled.div`
+     display: flex;
 `;
 
+const Card = styled.div`
+width: 300px;
+height: 200px;
 
-   const Card = styled.div`
-   width: 300px;
-   height: 200px;
-    
-   border-radius:  white; 
+border-radius:  white; 
 `;
-   
 
-   
- 
-      
-// velodeville.png
-          
-      const start = <i className="fas fa-star star-on"></i>
+const start = <i className="fas fa-star star-on"></i>
    
   return (
 
-   
       <>
        <Navbar />
 
